699274b776bdcc2e919deccd217d26ec73ca6377
zougui1 <35573266+zougui1@users.noreply.github.com>
2018-11-01 20:03:19 +0100

    update

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# TODO
 
-* home page: page with all images
-* search bar in home page, (search by, username/characterName/artistName)
-* design of everything! (home/user/upload/image)
-
-# priorities
-
-## can be done after official upload
-* home page
-* search bar in home page
-* re-design everything (complexe/complete)
-* replace the pagination with infinite scroll
--- src/components/Form/Overlay/Canvas.js
+++ src/components/Form/Overlay/Canvas.js
@@ -71,6 +71,33 @@ class Canvas extends React.Component {
         }
     }
 
+    drawArrow = (fromx, fromy, tox, toy) => {
+        const ctx = this.props.currentCanvasData.context;
+        //variables to be used when creating the arrow
+        var headlen = 6;
+        var angle = Math.atan2(toy - fromy, tox - fromx);
+        //starting path of the arrow from the start square to the end square and drawing the stroke
+        ctx.beginPath();
+        ctx.moveTo(fromx, fromy);
+        ctx.lineTo(tox, toy);
+        ctx.strokeStyle = "#cc0000";
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        //starting a new path from the head of the arrow to one of the sides of the point
+        ctx.beginPath();
+        ctx.moveTo(tox, toy);
+        ctx.lineTo(tox - headlen * Math.cos(angle - Math.PI / 7), toy - headlen * Math.sin(angle - Math.PI / 7));
+        //path from the side point of the arrow, to the other side point
+        ctx.lineTo(tox - headlen * Math.cos(angle + Math.PI / 7), toy - headlen * Math.sin(angle + Math.PI / 7));
+        //path from the side point back to the tip of the arrow, and then again to the opposite side point
+        ctx.lineTo(tox, toy);
+        ctx.lineTo(tox - headlen * Math.cos(angle - Math.PI / 7), toy - headlen * Math.sin(angle - Math.PI / 7));
+        //draws the paths created above
+        ctx.strokeStyle = "#cc0000";
+        ctx.lineWidth = 22;
+        ctx.stroke();
+    }
+
     mouseDownHandler = e => {
         const current = this.props.currentCanvasData;
 
@@ -86,7 +113,7 @@ class Canvas extends React.Component {
 
         if (!drawing) {return};
         changeCurrentCanvasData(current.drawing = false)
-        this.drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true);
+        this.drawArrow(current.x, current.y, e.clientX, e.clientY, current.color, true);
     }
 
     mouseMoveHandler = e => {
